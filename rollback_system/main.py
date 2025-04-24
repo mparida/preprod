c580a972015d99d73df3e039466efe4d728904da
mparida <mparida1000@gmail.com>
2025-04-24 13:15:29 +0530

    Update main.py

--- rollback_system/main.py
+++ rollback_system/main.py
@@ -1,1 +1,25 @@
 
+#!/usr/bin/env python3
+import argparse
+from core.rollback_processor import RollbackProcessor
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--release-branch', required=True)
+    parser.add_argument('--features', required=True)
+    parser.add_argument('--dry-run', action='store_true')
+    args = parser.parse_args()
+
+    processor = RollbackProcessor()
+    result = processor.rollback_features(
+        args.release_branch,
+        args.features.split(','),
+        args.dry_run
+    )
+    
+    # Save report
+    with open('../rollback_report.json', 'w') as f:
+        f.write(result.to_json())
+
+if __name__ == "__main__":
+    main()
